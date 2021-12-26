600aff0d31a89901c055106d3ff4d45d4092ce1c
Raghavendraprasad1 <raghavendrakumar1520@gmail.com>
2021-12-26 13:31:15 +0530

    export button added

--- application/views/admin/includes/admin_script.php
+++ application/views/admin/includes/admin_script.php
@@ -9,6 +9,13 @@
   <script src="<?= base_url('assets/admin/vendors/datatables.net-bs4/dataTables.bootstrap4.js'); ?>"></script>
   <!-- <script src="<?= base_url('assets/admin/js/dataTables.select.min.js'); ?>"></script> -->
   <script src="<?= base_url('assets/admin/js/jquery.dataTables.min.js'); ?>"></script>
+
+  <!-- datatable buttons plugins -->
+  <script src="https://cdn.datatables.net/buttons/2.1.0/js/dataTables.buttons.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.1.3/jszip.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/pdfmake.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/vfs_fonts.js"></script>
+  <script src="https://cdn.datatables.net/buttons/2.1.0/js/buttons.html5.min.js"></script>
   <!-- <script src="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/2.1.2/sweetalert.min.js"></script> -->
   <script src="<?= base_url('assets/admin/js/sweetalert2.all.min.js'); ?>"></script>
   <script src="<?= base_url('assets/admin/js/croppie.js'); ?>"></script>
--- assets/admin/js/general.js
+++ assets/admin/js/general.js
@@ -14,6 +14,50 @@ function alert_msg(type, msg) {
 }
 
 
+// code to export all rows from datatable record
+var oldExportAction = function (self, e, dt, button, config) {
+  if (button[0].className.indexOf('buttons-excel') >= 0) {
+      if ($.fn.dataTable.ext.buttons.excelHtml5.available(dt, config)) {
+          $.fn.dataTable.ext.buttons.excelHtml5.action.call(self, e, dt, button, config);
+      }
+      else {
+          $.fn.dataTable.ext.buttons.excelFlash.action.call(self, e, dt, button, config);
+      }
+  } else if (button[0].className.indexOf('buttons-print') >= 0) {
+      $.fn.dataTable.ext.buttons.print.action(e, dt, button, config);
+  }
+};
+
+var newExportAction = function (e, dt, button, config) {
+  var self = this;
+  var oldStart = dt.settings()[0]._iDisplayStart;
+
+  dt.one('preXhr', function (e, s, data) {
+      // Just this once, load all data from the server...
+      data.start = 0;
+      data.length = 2147483647;
+
+      dt.one('preDraw', function (e, settings) {
+          // Call the original action function 
+          oldExportAction(self, e, dt, button, config);
+          dt.one('preXhr', function (e, s, data) {
+              // DataTables thinks the first item displayed is index 0, but we're not drawing that.
+              // Set the property to what it was before exporting.
+              settings._iDisplayStart = oldStart;
+              data.start = oldStart;
+          });
+          // Reload the grid with the original page. Otherwise, API functions like table.cell(this) don't work properly.
+          setTimeout(dt.ajax.reload, 0);
+          // Prevent rendering of the full data to the DOM
+          return false;
+      });
+  });
+
+  // Requery the server with the new one-time export settings
+  dt.ajax.reload();
+};
+
+
 $("#firm_listing").DataTable({
   processing: true,
   serverSide: true,
@@ -245,6 +289,7 @@ $("#job_listing").DataTable({
   stateSave: true,
   scrollX: true,
   scrollCollapse: true,
+  dom: 'Bfrtip',
   fixedColumns: {
     leftColumns: 4,
   },
@@ -269,6 +314,12 @@ $("#job_listing").DataTable({
     [10, 25, 50, -1],
     [10, 25, 50, "All"],
   ],
+  buttons: [
+    {
+      extend: 'excel',
+      action: newExportAction
+    },
+  ]
 });
 
 
@@ -501,6 +552,7 @@ $("#jobEntriesList").DataTable({
     leftColumns: 6,
   },
   serverMethod: "post",
+  dom: 'Bfrtip',
   ajax: {
     url: admin_url + "Job/getJobEntries/" + $("#jobEntriesList").data("jobid"),
   },
@@ -519,6 +571,12 @@ $("#jobEntriesList").DataTable({
     [10, 25, 50, -1],
     [10, 25, 50, "All"],
   ],
+  buttons: [
+    {
+      extend: 'excel',
+      action: newExportAction
+    },
+  ]
 });
 
 
