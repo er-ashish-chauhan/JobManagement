67649833ee5470b92899d897609b2f0e2bdf2d5a
Ashish Chauhan <imashish58@gmail.com>
2022-01-16 12:12:13 +0530

    integrate pdf maker

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -58,7 +58,7 @@ $autoload['packages'] = array();
 |
 |	$autoload['libraries'] = array('user_agent' => 'ua');
 */
-$autoload['libraries'] = array('form_validation','encryption','session','database', 'pdf');
+$autoload['libraries'] = array('form_validation','encryption','session','database');
 
 /*
 | -------------------------------------------------------------------
