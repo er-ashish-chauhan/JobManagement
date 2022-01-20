04164d5f01ca47a93190dde9f402d51015e28d4d
Ashish Chauhan <imashish58@gmail.com>
2022-01-20 13:49:34 +0530

    integrate pdf maker

--- application/controllers/admin/Job.php
+++ application/controllers/admin/Job.php
@@ -318,8 +318,8 @@ class Job extends CI_Controller
 			$firmId = $this->input->post("bFirm");
 			$commodityId = $this->input->post("bCommodity");
 			$status = $this->input->post("bStatus");
-			$selectedDateFrom = $this->input->post("bSelectedDate");
-			$selectedDateto = $this->input->post("bSelectedDateTo") != "" ?
+			$selectedDateFrom = date("Y-m-d h:i:s", strtotime($this->input->post("bSelectedDate")));
+			$selectedDateto = date("Y-m-d h:i:s", strtotime($this->input->post("bSelectedDateTo"))) != "" ?
 				$this->input->post("bSelectedDateTo") : date("Y-m-d h:i:s");
 
 			$where = "WHERE `job`.`created` <= '$selectedDateto'";
