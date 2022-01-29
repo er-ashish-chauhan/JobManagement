adf416311e22ac3254a34e023df14bf96a0fe304
Ashish Chauhan <imashish58@gmail.com>
2022-01-29 22:13:02 +0530

    changes in entties

--- application/views/admin/view_entries.php
+++ application/views/admin/view_entries.php
@@ -2,29 +2,28 @@
 //  echo "<pre>";
 //  print_r($entry_details); die;
 
-    $entryType = "";
-    $deliveryType = "";
-    $commodity = "";
-    $firm_name = "";
-    $cNetWeight = "";
-    $bill ="";
-    $previousSlip = "";
-    $currentSlip = "";
-    $id = "";
+$entryType = "";
+$deliveryType = "";
+$commodity = "";
+$firm_name = "";
+$cNetWeight = "";
+$bill = "";
+$previousSlip = "";
+$currentSlip = "";
+$id = "";
 
-    if(!empty($entry_details))
-    {
-        $entryType = $entry_details->entryType;
-        $deliveryType = $entry_details->deliveryType;
-        $commodity = $entry_details->commodity;
-        $firm_name = $entry_details->firm_name;
-        $cNetWeight = $entry_details->cNetWeight;
-        $id = $entry_details->id;
-        //images
-        $bill = $entry_details->bill;
-        $previousSlip = $entry_details->previousSlip;
-        $currentSlip = $entry_details->currentSlip;
-    }
+if (!empty($entry_details)) {
+    $entryType = $entry_details->entryType;
+    $deliveryType = $entry_details->deliveryType;
+    $commodity = $entry_details->commodity;
+    $firm_name = $entry_details->firm_name;
+    $cNetWeight = $entry_details->cNetWeight;
+    $id = $entry_details->id;
+    //images
+    $bill = $entry_details->bill;
+    $previousSlip = $entry_details->previousSlip;
+    $currentSlip = $entry_details->currentSlip;
+}
 
 ?>
 <!-- partial -->
@@ -34,7 +33,7 @@
 
             <div class="row">
                 <div class="form-group col-sm-6 col-xs-12">
-                    <h4>Firm Name: </h4><?= $firm_name ?>  <?= $id ?>
+                    <h4>Firm Name: </h4><?= $firm_name ?> <?= $id ?>
                 </div>
                 <div class="form-group col-sm-6 col-xs-12">
                     <h4>Commodity: </h4><?= $commodity ?>
@@ -54,24 +53,24 @@
                 </div>
                 <div class="form-group col-sm-6 col-xs-12">
                     <h4>Bill-Image:</h4>
-                    <img src="<?= $bill ?>" alt="Bill-img">
+                    <img src="<?php base_url() . $bill ?>" alt="Bill-img">
                 </div>
             </div>
             <div class="row">
-            <div class="form-group col-sm-6 col-xs-12">
+                <div class="form-group col-sm-6 col-xs-12">
                     <h4>Previous-Slip:</h4>
-                    <img src="<?= $previousSlip ?>" alt="previous-slip">
+                    <img src="<?php base_url() . $previousSlip ?>" alt="previous-slip">
                 </div>
                 <div class="form-group col-sm-6 col-xs-12">
                     <h4>Current-Slip</h4>
-                    <img src="<?= $currentSlip ?>" alt="current-slip">
+                    <img src="<?php base_url() . $currentSlip ?>" alt="current-slip">
                 </div>
             </div>
             <!-- /.card-body -->
             <div class="card-footer">
                 <a href="<?= base_url('admin/entries'); ?>" class="btn btn-warning step-back float-left admin-cancel-btn" title="Cancel">Back</a>
-                <a href="<?= base_url('admin/entries/edit_entries_detail?id=').encode($id) ?>" class="btn btn-success btn-flat ml-1" title="Cancel">Edit details</a>
-                
+                <a href="<?= base_url('admin/entries/edit_entries_detail?id=') . encode($id) ?>" class="btn btn-success btn-flat ml-1" title="Cancel">Edit details</a>
+
             </div>
         </form>
     </div>
