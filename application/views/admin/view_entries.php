e4e97996b4c13a53891f31784cbcd3bb6ee249f3
Ashish Chauhan <imashish58@gmail.com>
2022-01-31 00:39:42 +0530

    changes in entties

--- application/views/admin/view_entries.php
+++ application/views/admin/view_entries.php
@@ -82,7 +82,17 @@ if (!empty($entry_details)) {
         </form>
     </div>
     <!-- content-wrapper ends -->
-
+      <!-- modal  -->
+      <div class="modal fade" id="imagemodal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-body">
+                    <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>
+                    <img src="" class="imagepreview" style="width: 100%;">
+                </div>
+            </div>
+        </div>
+    </div>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 
     <script>
