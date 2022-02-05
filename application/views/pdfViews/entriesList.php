402fb1f86eff9d0c013670c48c9759609825adac
Ashish Chauhan <imashish58@gmail.com>
2022-02-05 23:40:13 +0530

    filterChanges

--- application/views/pdfViews/entriesList.php
+++ application/views/pdfViews/entriesList.php
@@ -45,69 +45,28 @@
             <th>Firm</th>
         </tr>
         <?php
-        foreach ($entries as $value) {
-            echo "
-                <tr>
-                    <td>" . $value->BargainDetaiils . "</td>
-                    <td>" . $value->EntryDate . "</td>
-                    <td>" . $value->kantaSlipNo . "</td>
-                    <td>" . $value->TruckNo . "</td>
-                    <td>" . $value->Quantity_in_qts . "</td>
-                    <td>" . $value->Quantity_in_bags . "</td>
-                    <td>" . $value->FirmName . "</td>
-                    <td>" . $value->FirmAddress . "</td>
-                    <td>" . $value->userFirm . "</td>
-                 </tr>";
-        }
+        if ($entries) {
+            foreach ($entries as $value) {
+                echo "
+                    <tr>
+                        <td>" . $value->BargainDetaiils . "</td>
+                        <td>" . $value->EntryDate . "</td>
+                        <td>" . $value->kantaSlipNo . "</td>
+                        <td>" . $value->TruckNo . "</td>
+                        <td>" . $value->Quantity_in_qts . "</td>
+                        <td>" . $value->Quantity_in_bags . "</td>
+                        <td>" . $value->FirmName . "</td>
+                        <td>" . $value->FirmAddress . "</td>
+                        <td>" . $value->userFirm . "</td>
+                     </tr>";
+            }
+        } else { ?>
+            <tr>
+                <td colspan="9" style="align-self: center;">No entries for your selection!</td>
+            </tr>
+        <?php }
+
         ?>
-        <!-- <tr>
-            <td>Alfreds Futterkiste</td>
-            <td>Maria Anders</td>
-            <td>Germany</td>
-            <td>Germany</td>
-            <td>Germany</td>
-            <td>Germany</td>
-        </tr>
-        <tr>
-            <td>Centro comercial Moctezuma</td>
-            <td>Francisco Chang</td>
-            <td>Mexico</td>
-            <td>Mexico</td>
-            <td>Mexico</td>
-            <td>Mexico</td>
-        </tr>
-        <tr>
-            <td>Ernst Handel</td>
-            <td>Roland Mendel</td>
-            <td>Austria</td>
-            <td>Austria</td>
-            <td>Austria</td>
-            <td>Austria</td>
-        </tr>
-        <tr>
-            <td>Island Trading</td>
-            <td>Helen Bennett</td>
-            <td>UK</td>
-            <td>UK</td>
-            <td>UK</td>
-            <td>UK</td>
-        </tr>
-        <tr>
-            <td>Laughing Bacchus Winecellars</td>
-            <td>Yoshi Tannamuri</td>
-            <td>Canada</td>
-            <td>Canada</td>
-            <td>Canada</td>
-            <td>Canada</td>
-        </tr>
-        <tr>
-            <td>Magazzini Alimentari Riuniti</td>
-            <td>Giovanni Rovelli</td>
-            <td>Italy</td>
-            <td>Italy</td>
-            <td>Italy</td>
-            <td>Italy</td>
-        </tr> -->
     </table>
 </body>
 
